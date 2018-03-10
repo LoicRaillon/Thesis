a7e15a24070bcb03dbf3bf4ab6f9759924ee94f0
LoicRaillon <37119601+LoicRaillon@users.noreply.github.com>
2018-03-10 15:32:04 +0100

    Delete LLRatioTest.m

--- Chapter 4/utilities/LLRatioTest.m
+++
@@ -1,9 +0,0 @@
-function p = LLRatioTest(LL_large,LL_small,dof)
-
-small   = abs(LL_small);
-large   = abs(LL_large);
-ratio   = -2*(small-large);
-% p       = 1 - gammainc(ratio/2, dof/2);
-p       = 1 - pchisq(ratio, dof);
-
-
